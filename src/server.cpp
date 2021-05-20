c62fa8d3550719c99ee2c873e89598d665a3fdb1
Alexandru Cristian Jilavu <alexandru.jilavu@tremend.com>
2021-05-21 01:00:43 +0300

    POST endpoint for settings
    - implement new endpoint /settings which receives the new settings in the body
    - add files for every type of sensor
    - update sensor values when request is called
    - treat corner cases

--- src/server.cpp
+++ src/server.cpp
@@ -5,6 +5,8 @@
 #include "entities/groundSensor/GroundSensor.h"
 #include "entities/groundSensor/GroundNutrient.h"
 #include "libs/json.hpp"
+#include "entities/request/ChangeSensorSettings.h"
+#include "entities/sensor/SensorData.h"
 
 using nlohmann::json;
 
@@ -30,6 +32,7 @@ void Server::setupRoutes() {
     Rest::Routes::Get(router, "/testReadJson", Rest::Routes::bind(&Server::testReadJson, this));
     Rest::Routes::Get(router, "/testSaveJson", Rest::Routes::bind(&Server::testSaveJson, this));
     Rest::Routes::Get(router, "/groundSensor", Rest::Routes::bind(&Server::groundSensorJson, this));
+    Rest::Routes::Post(router, "/settings", Rest::Routes::bind(&Server::changeSettings, this));
 
 }
 
@@ -57,4 +60,42 @@ void Server::groundSensorJson(const Rest::Request &request, Http::ResponseWriter
 
     GroundSensor groundData = GroundSensor(JSONUtils::readJsonFromFile(Constants::PROJECT_SRC_ROOT + Constants::GROUND_SENSOR_PATH));
     response.send(Pistache::Http::Code::Ok, std::to_string(groundData.getNutrient().getValue()));
+}
+
+void Server::changeSettings(const Rest::Request &request, Http::ResponseWriter response) {
+    ChangeSensorSettings req = ChangeSensorSettings(nlohmann::json::parse(request.body()));
+    std::string filePath;
+    switch (req.getSensorType()) {
+        case SensorType::HUMIDITY:
+            filePath = Constants::HUMIDITY_SENSOR_PATH;
+            break;
+        case SensorType::LIGHT:
+            filePath = Constants::LIGHT_SENSOR_PATH;
+            break;
+        case SensorType::TEMPERATURE:
+            filePath = Constants::TEMPERATURE_SENSOR_PATH;
+            break;
+        case SensorType::FERTILIZER:
+            filePath = Constants::FERTILIZER_SENSOR_PATH;
+            break;
+        default:
+            response.send(Pistache::Http::Code::Not_Acceptable, "The sensorType provided is not valid");
+            return;
+    }
+    SensorData sensor = SensorData(JSONUtils::readJsonFromFile(Constants::PROJECT_SRC_ROOT + filePath));
+
+    if (sensor.getMaxValue() == req.getMaxValue() && sensor.getMinValue() == req.getMinValue()) {
+        response.send(Pistache::Http::Code::Not_Modified, "The same values are already set!");
+        return;
+    }
+
+    if (req.getMinValue() > req.getMaxValue()) {
+        response.send(Pistache::Http::Code::Not_Acceptable, "MinValue must be lower that maxValue!");
+        return;
+    }
+
+    sensor.update(req);
+    JSONUtils::writeJsonToFile(Constants::PROJECT_SRC_ROOT + filePath, sensor.to_json().dump(4));
+
+    response.send(Pistache::Http::Code::Ok, "Success");
 }
